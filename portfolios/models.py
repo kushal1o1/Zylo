78754244a8fa43b82c7fee6539a9410f1a4af8fa
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-09-28 21:43:47 +0545

    Create Models(Just for initial tests)

--- portfolios/models.py
+++ portfolios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+
+class ChatQuestion(models.Model):
+    text = models.TextField(help_text="Enter the question text.")
+    is_active = models.BooleanField(default=True, help_text="Indicates if the question is active.")
+    
+    def __str__(self):
+        return self.text
+
+class ChatSession(models.Model):
+    answer = models.TextField(help_text="Store user's answer.")
+    question = models.ForeignKey(ChatQuestion, on_delete=models.CASCADE, related_name='responses')
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Time when the answer was recorded.")
+    
+    def __str__(self):
+        return f"Answer to '{self.question.text}': {self.answer}"
