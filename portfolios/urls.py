a7078a7644e3362572a58fa78d7abf7459f603e8
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-11-05 20:54:21 +0545

    fix:Urls routing

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -9,13 +9,15 @@ from .views import update_user_info,update_images
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path("NotFound/",views.NotFound,name="NotFound"),
-    path('<str:userUrl>/',views.index,name="index"),
     path('create/',views.home,name="home"),
+    path('update_user_info/', update_user_info, name='update_user_info'),
+    path('update_images/', update_images, name='update_images'),
+    path('<str:userUrl>/',views.index,name="index"),
+  
 
     
     
     # path('chat/', ChatView.as_view(), name='chat_view'),
-     path('update_user_info/', update_user_info, name='update_user_info'),
-     path('update_images/', update_images, name='update_images'),
+     
      
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
