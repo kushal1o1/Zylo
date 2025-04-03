70689749eb686166bfc2d7a2458e9f84f7d0c02b
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-03 19:15:54 +0545

    TODO: added new todos

---
+++ TODO.py
@@ -0,0 +1,26 @@
+# TODO: CReate a landing page for the project
+done = False
+
+# TODO: MAke professional emails 
+done = False
+
+# TODO: Add gmail ,fb or other social media login
+done = False
+
+# TODO: Add a contact us page
+done = False
+
+# TODO: Add a about us page\
+done = False
+
+# TODO:Make Professional templates 
+done = False
+
+# TODO :Add new wmail Sending service for new  features informing from admin pannel
+done = False
+
+# TODO: Add a Better apporach for updating preview instead of debouncing the whole page
+done = False
+
+# TODO: Add a better approach for error handling and showing error messages to the user
+done = False
