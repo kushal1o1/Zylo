799efa1723c0b8059f267b76bd116460c1f5720b
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-06 22:46:49 +0545

    fix:spaces and maintain clean comment

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -11,13 +10,14 @@ urlpatterns = [
     path('create/',views.home,name="home"),
     path('update_user_info/', update_user_info, name='update_user_info'),
     path('update_images/', update_images, name='update_images'),
+    
+    # Url for updating background image
     path('update_background_image/', update_background_image, name='update_background_image'),
     path('delete_background_image/', delete_background_image, name='delete_background_image'),
-    
-    
 
-    
+    # Urls for handling highlights
     path('highlights/delete/<int:highlight_id>/', views.delete_highlight, name='delete_highlight'),  # Delete highlight
+
 
     path('sections/delete/<int:section_id>/', views.delete_section, name='delete_section'),
     path('delete-section-data/<int:data_id>/', views.delete_section_data, name='delete_section_data'),
