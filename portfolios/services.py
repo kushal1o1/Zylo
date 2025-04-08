7c386b8e2c4a5a019265d1c30d4a2a0d4c1f2f38
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-04-08 17:12:32 +0545

    Feat:Create background data dynamically instead of hardcoding from model

--- portfolios/services.py
+++ portfolios/services.py
@@ -1,28 +1,47 @@
 from .models import UserInfo,Highlight,Section,SectionData,Alldesigns
-background_templates = {
-        "bg0": "designs/default.html",
-        "bg1": "designs/bg2.html",
-        "bg2": "designs/bigbox.html",
-        "bg3": "designs/checkboard.html",
-        "bg4": "designs/dotted.html",
-        "bg5": "designs/dottedbg.html",
-        "bg6": "designs/graphbigboxes.html",
-        "bg7": "designs/overlay.html",
-        "bg8": "designs/test.html",
-        "bg9": "animated/canvas.html",
-        "bg10": "animated/circle.html",
-        "bg11": "animated/colorcircle.html",
-        "bg12": "animated/diagonalstrip.html",
-        "bg13": "animated/hexagon.html",
-        "bg14": "animated/polygon.html",
-        "bg15": "animated/star.html",
-        "bg16": "animated/test.html",
-        "bg17": "animated/particles.html",
-        "bg18": "designs/Wavyy.html",
-        "bg19": "designs/polygon.html",
-        "bg20": "designs/Celebration.html",
-        }
+# background_templates = {
+#         "bg0": "designs/default.html",
+#         "bg1": "designs/bg2.html",
+#         "bg2": "designs/bigbox.html",
+#         "bg3": "designs/checkboard.html",
+#         "bg4": "designs/dotted.html",
+#         "bg5": "designs/dottedbg.html",
+#         "bg6": "designs/graphbigboxes.html",
+#         "bg7": "designs/overlay.html",
+#         "bg8": "designs/test.html",
+#         "bg9": "animated/canvas.html",
+#         "bg10": "animated/circle.html",
+#         "bg11": "animated/colorcircle.html",
+#         "bg12": "animated/diagonalstrip.html",
+#         "bg13": "animated/hexagon.html",
+#         "bg14": "animated/polygon.html",
+#         "bg15": "animated/star.html",
+#         "bg16": "animated/test.html",
+#         "bg17": "animated/particles.html",
+#         "bg18": "designs/Wavyy.html",
+#         "bg19": "designs/polygon.html",
+#         "bg20": "designs/Celebration.html",
+#         }
+background_templates = {}
+background_names = {}
+def get_background_data():
 
+    
+    # Retrieve all designs from the database
+    designs = Alldesigns.objects.all()
+
+    # Loop through the designs and build the dictionaries
+    for design in designs:
+        # Create the template path (based on the location selected in the admin)
+        file_path = f"{design.location}/{design.template_file.name.split('/')[-1]}"
+        
+        # Add to the dictionary using the code_name as key
+        background_templates[design.code_name] = file_path
+        background_names[design.code_name] = design.name
+
+    return background_templates, background_names
+
+background_templates, background_names = get_background_data()
 
 def get_user_data(user_info):
     """
@@ -58,20 +77,4 @@ def handle_background_image_form(background_image_form, user):
         return True
     return False
 
-def get_background_data():
-    background_templates = {}
-    background_names = {}
-    
-    # Retrieve all designs from the database
-    designs = Alldesigns.objects.all()
 
-    # Loop through the designs and build the dictionaries
-    for design in designs:
-        # Create the template path (based on the location selected in the admin)
-        file_path = f"{design.location}/{design.template_file.name.split('/')[-1]}"
-        
-        # Add to the dictionary using the code_name as key
-        background_templates[design.code_name] = file_path
-        background_names[design.code_name] = design.name
-
-    return background_templates, background_names
